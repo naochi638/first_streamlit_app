94cfbfd4c5e917bd8e66c73a0f7ab8bbaf6e2725
Naomi Chin <100549983+naochi638@users.noreply.github.com>
2022-11-21 16:01:49 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit') # index is set to fruit for the multiselect thing
 
 
-streamlit.title("My Prents' New Healthy Diner")
+streamlit.title("My Mum's New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -37,7 +37,7 @@ streamlit.dataframe(fruityvice_normalized) # outputs it as a table/dataframe
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall() # can do fetchone
 streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_rows)
 
